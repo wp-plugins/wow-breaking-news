db14c8c07e110d1c9476b32396d7346d40570a5e
StoffeTiX <StoffeTiX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-01 20:48:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wow-breaking-news/trunk@188754 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wow-breaking-news.php
@@ -0,0 +1,90 @@
+<?php
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+Plugin Name: WoW Breaking News
+Plugin URI: http://www.tixdesign.com/wow-breaking-news
+Description: World of Warcraft Breaking News Displaying Widget
+Author: StoffeTiX
+Version: 1.2
+Author URI: http://www.tixdesign.com/
+*/
+
+function samplewowbreakingnews()
+{
+$fp = fopen("http://status.wow-europe.com/en/alert", "r");
+$data = fread($fp, 1000);
+fclose($fp);
+
+//Show if no news
+if (strlen($data)<=1) { 
+echo "* There are no alerts at this time.".$thelink;
+} else {
+echo $data;
+}
+}
+
+function widget_wowbreakingnews($args) {
+  extract($args);
+
+  $options = get_option("widget_wowbreakingnews");
+  if (!is_array( $options ))
+        {
+                $options = array(
+      'title' => 'WoW Breaking News'
+      );
+  }      
+
+  echo $before_widget;
+    echo $before_title;
+      echo $options['title'];
+    echo $after_title;
+
+    //Widget Content
+    samplewowbreakingnews();
+  echo $after_widget;
+}
+
+function wowbreakingnews_control()
+{
+  $options = get_option("widget_wowbreakingnews");
+  if (!is_array( $options ))
+        {
+                $options = array(
+      'title' => 'WoW Breaking News'
+      );
+  }    
+
+  if ($_POST['wowbreakingnews-Submit'])
+  {
+    $options['title'] = htmlspecialchars($_POST['wowbreakingnews-WidgetTitle']);
+    update_option("widget_wowbreakingnews", $options);
+  }
+
+?>
+  <p>
+    <label for="wowbreakingnews-WidgetTitle">Widget Title: </label>
+    <input type="text" id="wowbreakingnews-WidgetTitle" name="wowbreakingnews-WidgetTitle" value="<?php echo $options['title'];?>" />
+    <input type="hidden" id="wowbreakingnews-Submit" name="wowbreakingnews-Submit" value="1" />
+  </p>
+<?php
+}
+
+function wowbreakingnews_init()
+{
+  register_sidebar_widget(__('WoW Breaking News'), 'widget_wowbreakingnews');
+  register_widget_control(   'WoW Breaking News', 'wowbreakingnews_control', 300, 200 );    
+}
+add_action("plugins_loaded", "wowbreakingnews_init");
+?>
